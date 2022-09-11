c2e744411fbbf8d1637569c6840a28107c5c3d71
HuseynJafarov <huseynnj@code.edu.az>
2022-09-11 18:02:09 +0400

    Home-Page

---
+++ assets/js/home.js
@@ -0,0 +1,23 @@
+$(function () {
+
+ 
+
+
+
+});
+
+
+function openTabMenu(evt, TabMenu) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabMenu).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+///home-tab-slider
