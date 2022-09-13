0855c574778284c975bd843785524c306e4d7960
HuseynJafarov <huseynnj@code.edu.az>
2022-09-13 21:38:51 +0400

    Login-Register

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,67 +1,32 @@
-$(function () {});
-var swiper = new Swiper(" #intro-slider .mySwiper", {
-  loop:true,
+$(function() {
+  let swiper = new Swiper(".mySwiper", {
+    loop: true,
+  });
+  
+  $('input').focus(function(){
+    $(this).parents('.form-group').addClass('focused');
+  });
+  
+  $('input').blur(function(){
+    var inputValue = $(this).val();
+    if ( inputValue == "" ) {
+      $(this).removeClass('filled');
+      $(this).parents('.form-group').removeClass('focused');  
+    } else {
+      $(this).addClass('filled');
+    }
+  })
 
-});
+  let scrollSection = document.getElementById("scroll-section");
+   
+   window.onscroll = function() {scrollFunction()};
 
-
-function openTabMenu(evt, TabMenu) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(TabMenu).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-///home-tab-slider
-
-
-var swiper1 = new Swiper("#tab-menu .mySwiper1", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-
-  autoplay: {
-    delay: 2000,
-    
-  },
-
-});
-
-var swiper2 = new Swiper("#tab-menu .mySwiper2", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 2000,
-    
-  },
- 
-});
-
-var swiper3 = new Swiper("#tab-menu .mySwiper3", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 2000,
-    
-  },
-
-});
-
-var swiper4 = new Swiper("#tab-menu .mySwiper4", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-
-  autoplay: {
-    delay: 2000,
-    
-  },
-});
+   function scrollFunction() {
+     if (document.body.scrollTop > 195|| document.documentElement.scrollTop > 195) {
+        scrollSection.style.top = "0";
+     } else {
+        scrollSection.style.top = "-62px";
+        $("div").removeClass("inActive");
+     }
+   }
+})
