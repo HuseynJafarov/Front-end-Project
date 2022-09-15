d06e9664b102afc2e65a58aadf0fa2a0d379b296
HuseynJafarov <huseynnj@code.edu.az>
2022-09-15 21:49:32 +0400

    accessories

--- assets/js/home.js
+++ assets/js/home.js
@@ -30,3 +30,64 @@ $(function() {
      }
    }
 })
+
+function openTabMenu(evt, TabMenu) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabMenu).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+var swiper1 = new Swiper("#tab-menu .mySwiper1", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+});
+
+var swiper3 = new Swiper("#tab-menu .mySwiper3", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+
+});
+
+var swiper4 = new Swiper("#tab-menu .mySwiper4", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView:5,
+  loop:true,
+  spaceBetween:20,
+  autoplay:{
+      delay:3000,
+      disableOnInteraction:false,
+  },
+  breakpoints:{
+      0:{
+          slidesPerView:1,
+      },
+      550:{
+          slidesPerView:2,
+      },
+      800:{
+          slidesPerView:4,
+      },
+      1000:{
+          slidesPerView:5,
+      },
+  },
+  pagination:{
+      el:".swiper-pagination",
+      clickable:true,
+  }
+});
