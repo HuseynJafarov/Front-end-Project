9efb341c965e0c33b06a514898b8a9817aa3fdae
HuseynJafarov <huseynnj@code.edu.az>
2022-09-19 00:49:45 +0400

    Cart

--- assets/js/accessories.js
+++ assets/js/accessories.js
@@ -1,27 +1,82 @@
-$(function() {
+$(function () {
+  let scrollSection = document.getElementById("scroll-section");
 
-    let scrollSection = document.getElementById("scroll-section");
-   
-     window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-    function scrollFunction() {
-     if (document.body.scrollTop > 195|| document.documentElement.scrollTop > 195) {
-        scrollSection.style.top = "0";
-     } else {
-        scrollSection.style.top = "-62px";
-        $("div").removeClass("inActive");
-     }
-   }
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 195 ||
+      document.documentElement.scrollTop > 195
+    ) {
+      scrollSection.style.top = "0";
+    } else {
+      scrollSection.style.top = "-62px";
+      $("div").removeClass("inActive");
+    }
+  }
 
+  // $('#singup input').blur(function(){
+  //    var inputValue = $(this).val();
+  //    if ( inputValue == "" ) {
+  //      $(this).removeClass('filled');
+  //      $(this).parents('.input').removeClass('focused');
+  //    } else {
+  //      $(this).addClass('filled');
+  //    }
+  //  })
+});
 
-   // $('#singup input').blur(function(){
-   //    var inputValue = $(this).val();
-   //    if ( inputValue == "" ) {
-   //      $(this).removeClass('filled');
-   //      $(this).parents('.input').removeClass('focused');  
-   //    } else {
-   //      $(this).addClass('filled');
-   //    }
-   //  })
+let basketBtns = document.querySelectorAll("#accessories .shop-icon");
 
-})
+let products = [];
+
+if (localStorage.getItem("products") != null) {
+  products = JSON.parse(localStorage.getItem("products"));
+}
+
+basketBtns.forEach((basketBtn) => {
+  basketBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let productImage =
+      this.parentNode.previousElementSibling.lastElementChild.getAttribute("src");
+    
+    let productName = this.parentNode.firstElementChild.innerText;
+    let productPrice = parseInt(this.nextElementSibling.innerText.substring(2));
+    let productId = parseInt(
+       this.parentNode.parentNode.getAttribute("data-id")
+       );
+      
+
+    let existProduct = products.find((m) => m.id == productId);
+    console.log(productId);
+
+    if (existProduct != undefined) {
+      existProduct.count += 1;
+    } else {
+      products.push({
+        id: productId,
+        name: productName,
+        price:productPrice,
+        image: productImage,
+        count: 1,
+      });
+    }
+
+    localStorage.setItem("products", JSON.stringify(products));
+    document.querySelector("sup").innerText = getProductsCount(products);
+  });
+});
+
+document.querySelector("sup").innerText = getProductsCount(products);
+
+function getProductsCount(items) {
+  let resultCount = 0;
+  for (const item of items) {
+    resultCount += item.count;
+  }
+  return resultCount;
+}
+  
--- assets/js/cart.js
+++ assets/js/cart.js
@@ -18,3 +18,63 @@ $(function () {
     }
     
   });
+
+
+  products = [];
+
+if (localStorage.getItem("products") != null) {
+  products = JSON.parse(localStorage.getItem("products"));
+}
+
+let tableBody = document.querySelector("#basket .table .table-body");
+
+addDatasToTable(products);
+showIconBasketCount();
+let deleteBtns = document.querySelectorAll(".delete-btn");
+
+deleteBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+
+
+    deleteProducts(this);
+
+    
+  });
+});
+
+function deleteProducts(btn) {
+  let id = parseInt(
+    btn.parentNode.parentNode.firstElementChild.getAttribute("data-id")
+  );
+  products = products.filter((m) => m.id != id);
+  localStorage.setItem("products", JSON.stringify(products));
+  // document.location.reload();
+  btn.parentNode.parentNode.remove();
+  showIconBasketCount();
+}
+
+function addDatasToTable(products) {
+  for (const product of products) {
+    tableBody.innerHTML += `
+          <tr>
+          <td data-id ="${product.id}"><img src="${product.image}" class="card-img-top" alt="..."></td>
+          <td>${product.name}</td>
+          <td>${product.price} ${"$"}</td>
+          <td>${product.count}</td>
+          <td>${product.id}</td>
+          <td><i class="fa-solid fa-trash delete-btn" style="color: red; cursor: pointer;"></i></td>
+        </tr>`;
+  }
+}
+
+function getProductsCount(items) {
+  let resultCount = 0;
+  for (const item of items) {
+    resultCount += item.count;
+  }
+  return resultCount;
+}
+function showIconBasketCount() {
+  document.querySelector(".sup sup").innerText = getProductsCount(products);
+}
+
