7b1d6f4539c313c952d8e1710453c820394528fe
HuseynJafarov <huseynnj@code.edu.az>
2022-09-19 04:24:34 +0400

    Basket

--- assets/js/aboutUs.js
+++ assets/js/aboutUs.js
@@ -50,3 +50,4 @@ $(function () {
          clickable:true,
      }
    });
+
--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,31 +1,30 @@
 $(function () {
-    let scrollSection = document.getElementById("scroll-section");
-  
-    window.onscroll = function () {
-      scrollFunction();
-    };
-  
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 195 ||
-        document.documentElement.scrollTop > 195
-      ) {
-        scrollSection.style.top = "0";
-      } else {
-        scrollSection.style.top = "-62px";
-        $("div").removeClass("inActive");
-      }
+  let scrollSection = document.getElementById("scroll-section");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 195 ||
+      document.documentElement.scrollTop > 195
+    ) {
+      scrollSection.style.top = "0";
+    } else {
+      scrollSection.style.top = "-62px";
+      $("div").removeClass("inActive");
     }
-    
-  });
+  }
+});
 
-
-  products = [];
+products = [];
 
 if (localStorage.getItem("products") != null) {
   products = JSON.parse(localStorage.getItem("products"));
 }
 
+let totalBody = document.querySelector("#table .table-total .table-body")
 let tableBody = document.querySelector("#basket .table .table-body");
 
 addDatasToTable(products);
@@ -34,11 +33,7 @@ let deleteBtns = document.querySelectorAll(".delete-btn");
 
 deleteBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
-
-
     deleteProducts(this);
-
-    
   });
 });
 
@@ -55,17 +50,20 @@ function deleteProducts(btn) {
 
 function addDatasToTable(products) {
   for (const product of products) {
+    
     tableBody.innerHTML += `
           <tr>
           <td data-id ="${product.id}"><img src="${product.image}" class="card-img-top" alt="..."></td>
           <td>${product.name}</td>
           <td>${product.price} ${"$"}</td>
-          <td>${product.count}</td>
-          <td>${product.id}</td>
+          <td>${product.count}</td> 
+          <td>${product.price * product.count} ${"$"}</td>
           <td><i class="fa-solid fa-trash delete-btn" style="color: red; cursor: pointer;"></i></td>
         </tr>`;
   }
+
 }
+
 
 function getProductsCount(items) {
   let resultCount = 0;
@@ -77,4 +75,3 @@ function getProductsCount(items) {
 function showIconBasketCount() {
   document.querySelector(".sup sup").innerText = getProductsCount(products);
 }
-
